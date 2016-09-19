3d17783f6869639365c97c4a1f88090a472b55e5
Christianbegines <christianbeginescarrasco@gmail.com>
2016-09-19 22:40:05 +0200

    Primera pagina con jade y express

--- hola_express/app.js
+++ hola_express/app.js
@@ -1,9 +1,9 @@
 var express=require("express");
 
-var app =expres(); 
+var app =express(); 
 app.set("view engine","jade");
 app.get("/",function(require,response){
-    response.send("Hola mundo");
+    response.render("index",{hola:"Hola chavo"});
 });
 
 app.listen(8080);
