dec4291700e2b4c70c91ed2690f8599831526451
Christianbegines <christianbeginescarrasco@gmail.com>
2016-09-22 17:24:58 +0200

    Creando la base de datos en MongoDb

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,6 +1,24 @@
 var express = require("express");
 var bodyParser=require("body-parser");
 var app=express();
+var mongoose=require("mongoose");
+var mpromise=require("mpromise");
+var Schema=mongoose.Schema;
+
+mongoose.Promise= global.Promise;
+
+
+mongoose.connect("mongodb://localhost/fotos");
+
+var userSchemaJSON = {
+	email:String,
+	password:String
+};
+
+var user_schema=new Schema(userSchemaJSON);
+var User=mongoose.model("User",user_schema)
+
+
 
 //para utilizar el middleware de built-in
 app.use("/public",express.static('public')); 
@@ -13,14 +31,21 @@ app.get("/",function(request,response){
 	response.render("index");
 });
 app.get("/login",function(request,response){
+	User.find(function(err,doc){
+		console.log(doc);
+		response.render("login");
+	})
 	
-	response.render("login");
 });
 
 app.post("/users",function(request,response){
-	console.log("Contraseña:"+ request.body.password);
-	console.log("Email:"+ request.body.email);
-	response.send("Recibimos tus datos");
+	var user = new User({email: request.body.email, password: request.body.password})
+	user.save(function(){
+		response.send("Guardamos tus datos");
+	});
+		
+	
+	
 });
 
 app.listen(8080);
