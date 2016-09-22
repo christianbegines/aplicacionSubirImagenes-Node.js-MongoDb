86949742944063f4a2444e462383e85a2678dda9
Christianbegines <christianbeginescarrasco@gmail.com>
2016-09-22 16:16:04 +0200

    Recibiendo datos de un formulario

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,11 +1,26 @@
 var express = require("express");
+var bodyParser=require("body-parser");
 var app=express();
+
+//para utilizar el middleware de built-in
+app.use("/public",express.static('public')); 
+app.use(bodyParser.json());//para peticiones tipo json
+app.use(bodyParser.urlencoded({extended:true}));//para parsear urls
 app.set("view engine","jade");
-app.get("/",function(require,response){
+
+
+app.get("/",function(request,response){
 	response.render("index");
 });
-app.get("/login",function(require,response){
+app.get("/login",function(request,response){
+	
 	response.render("login");
+});
+
+app.post("/users",function(request,response){
+	console.log("Contraseña:"+ request.body.password);
+	console.log("Email:"+ request.body.email);
+	response.send("Recibimos tus datos");
 });
 
 app.listen(8080);
