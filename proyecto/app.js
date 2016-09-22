1284a2f4652c9be33a666cffb8df7bf701fc7db4
Christianbegines <christianbeginescarrasco@gmail.com>
2016-09-22 17:40:39 +0200

    Configurando el modulo de la base de datos

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,24 +1,8 @@
 var express = require("express");
 var bodyParser=require("body-parser");
+var user=require("./models/user").User;
+
 var app=express();
-var mongoose=require("mongoose");
-var mpromise=require("mpromise");
-var Schema=mongoose.Schema;
-
-mongoose.Promise= global.Promise;
-
-
-mongoose.connect("mongodb://localhost/fotos");
-
-var userSchemaJSON = {
-	email:String,
-	password:String
-};
-
-var user_schema=new Schema(userSchemaJSON);
-var User=mongoose.model("User",user_schema)
-
-
 
 //para utilizar el middleware de built-in
 app.use("/public",express.static('public')); 
