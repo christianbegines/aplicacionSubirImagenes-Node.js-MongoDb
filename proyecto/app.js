747a28e6dd137136af3b5b826443e385cc1c93a0
Christianbegines <christianbeginescarrasco@gmail.com>
2016-09-24 21:09:02 +0200

    Arreglos varios

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,16 +1,15 @@
 var express = require("express");
 var bodyParser=require("body-parser");
 var User=require("./models/users").User;
-
 var app=express();
-
 //para utilizar el middleware de built-in
 app.use("/public",express.static('public')); 
 app.use(bodyParser.json());//para peticiones tipo json
 app.use(bodyParser.urlencoded({extended:true}));//para parsear urls
+//añade el jade
 app.set("view engine","jade");
 
-
+//recoge las peticiones de servidor
 app.get("/",function(request,response){
 	response.render("index");
 });
@@ -19,14 +18,14 @@ app.get("/signup",function(request,response){
 		console.log(doc);
 
 		response.render("signup");
-	})
-	
+	})	
 });
 
 app.get("/login",function(request,response){
 	response.render("login");	
 });
 
+//devuelven las correspondientes paginas con su funcion
 app.post("/users",function(request,response){
 	var user = new User({
 						email: request.body.email, 
@@ -34,26 +33,18 @@ app.post("/users",function(request,response){
 						password_confirmation: request.body.password_confirmation,
 						username: request.body.username
 	});
-
 	user.save().then(function(us){
 		response.send("guardamos sus datos exitosamente");
 	},function(err){
 		if(err){
 			console.log(String(err));
 			response.send("No pudimos guardar la informacion");
-		}
-	  
-	});
-		
-	
-	
+		}  
+	});	
 });
 
-app.post("/sessions",function(request,response){
-	
-		
-	
-	
+app.post("/sessions",function(request,response){	
+
 });
 
 app.listen(8080);
