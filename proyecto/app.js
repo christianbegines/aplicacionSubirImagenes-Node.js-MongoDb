705ed8e51f9f57cac092f2b1dfdfeba7fdd2bc5d
Christianbegines <christianbeginescarrasco@gmail.com>
2016-10-13 17:11:55 +0200

    Crea el id de sesion

--- proyecto/app.js
+++ proyecto/app.js
@@ -3,7 +3,7 @@ var bodyParser=require("body-parser");
 var User=require("./models/users").User;
 var app=express();
 //para utilizar el middleware de built-in
-app.use("/public",express.static('public')); 
+app.use("/public",express.static('public'));
 app.use(bodyParser.json());//para peticiones tipo json
 app.use(bodyParser.urlencoded({extended:true}));//para parsear urls
 //añade el jade
@@ -17,34 +17,34 @@ app.get("/signup",function(request,response){
 	User.find(function(err,doc){
 		console.log(doc);
 		response.render("signup");
-	})	
+	})
 });
 
 app.get("/login",function(request,response){
-	response.render("login");	
+	response.render("login");
 });
 
 //devuelven las correspondientes paginas con su funcion
 app.post("/users",function(request,response){
 	var user = new User({
-						email:request.body.email, 
+						email:request.body.email,
 						password:request.body.password,
 						password_confirmation:request.body.password_confirmation,
 						username:request.body.username
 	});
 	user.save().then(function(us){
 		response.send("guardamos sus datos exitosamente");
-	},function(err){		
+	},function(err){
 			console.log(String(err));
-			response.send("No pudimos guardar la informacion");		  
-	});	
+			response.send("No pudimos guardar la informacion");
+	});
 });
 
-app.post("/sessions",function(request,response){	
+app.post("/sessions",function(request,response){
 	User.findOne({email:request.body.email,password:request.body.password},function(err,docs
 		){
 		console.log(docs);
-		response.send("Hola Chavo");
+		response.send("Se a logueado correctamente");
 
 		})
 });
