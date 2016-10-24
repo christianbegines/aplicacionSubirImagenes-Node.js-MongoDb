1e1e0122275be6726a78fb48b3f9acd31e667a09
Christianbegines <christianbeginescarrasco@gmail.com>
2016-10-24 17:15:26 +0200

    Empezando con la primera funcionalidad

--- proyecto/router_app.js
+++ proyecto/router_app.js
@@ -5,5 +5,31 @@ router.get("/",function(require,response){
 /*Buscar  usuario*/
   response.render("app/home")
 });
+/*REST*/
+
+router.get("/imagenes/new",function(request,response){
+  response.render("app/imagenes/new");
+});
+router.get("/imagenes/:id/edit",function(request,response){
+
+});
+router.route("/imagenes/:id")
+  .get(function(request,response){
+
+  })
+  .put(function(request,response){
+
+  })
+  .delete(function(request,response){
+
+  });
+
+router.route("/imagenes")
+  .get(function(request,response){
+
+  })
+  .put(function(request,response){
+
+  });
 
 module.exports=router;
